15932a34c8ccc50b8fb518be9e16d860bdd38ba5
Poorvi52 <74486589+Poorvi52@users.noreply.github.com>
2022-02-02 21:06:07 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,7 @@
+print("Number guessing Game")
+print("Guess a number between 1-9")
+a=int(input("Enter your guess:-"))
+if (a>9):
+    print("Guess a number higher than that!")
+elif (a==9):   
+    print("Yay! You won") 
